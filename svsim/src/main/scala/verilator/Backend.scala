7b2f3c61eecbc7f2158795a39880bad728660e40
Kamyar Mohajerani <kammoh@gmail.com>
2025-04-29 23:59:36 -0400

    unreleated

--- svsim/src/main/scala/verilator/Backend.scala
+++ svsim/src/main/scala/verilator/Backend.scala
@@ -73,7 +73,8 @@ object Backend {
     outputSplitCFuncs:          Option[Int] = None,
     disabledWarnings:           Seq[String] = Seq(),
     disableFatalExitOnWarnings: Boolean = false,
-    enableAllAssertions:        Boolean = false
+    enableAllAssertions:        Boolean = false,
+    additionalArguments:        Seq[String] = Seq()
   ) extends svsim.Backend.Settings
 
   def initializeFromProcessEnvironment() = {
@@ -198,6 +199,8 @@ final class Backend(executablePath: String) extends svsim.Backend {
               Seq(flag, value.mkString(" "))
             }
           }.flatten,
+
+          backendSpecificSettings.additionalArguments,
 
           Seq(
             commonSettings.verilogPreprocessorDefines,
